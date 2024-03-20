cb8cb54f9da2f697eda5aca229c156825b4fc7bb
raihank43 <raihankusuma5@gmail.com>
2024-03-20 17:15:01 +0700

    push

--- views/DirectMessage.jsx
+++ views/DirectMessage.jsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
-import axios from '../utils/axios';
-import { Link, useParams } from 'react-router-dom';
-import { socket } from '../src/socket'; // tambahkan ini di bagian atas file
-import toastMsgNotif from '../utils/toastMsgNotif';
-import Sidebar from '../src/components/Sidebar';
-import IncomingMessage from '../src/components/IncomingMessage';
-import OutgoingMessage from '../src/components/OutgoingMessage';
-import showToastSuccess from '../utils/toastSucces';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import axios from "../utils/axios";
+import { Link, useParams } from "react-router-dom";
+import { socket } from "../src/socket"; // tambahkan ini di bagian atas file
+import toastMsgNotif from "../utils/toastMsgNotif";
+import Sidebar from "../src/components/Sidebar";
+import IncomingMessage from "../src/components/IncomingMessage";
+import OutgoingMessage from "../src/components/OutgoingMessage";
+import showToastSuccess from "../utils/toastSucces";
+import { useDispatch, useSelector } from "react-redux";
 import {
   deletePrivMessageById,
   fetchDirectMessages,
   sendPrivMessage,
   setMessage,
-} from '../src/features/DirectMessage/DirectMessageSlice';
-import { findProfiles } from '../src/features/Profile/FindUsernameByProfileSlice';
-import { fetchLoggedProfile } from '../src/features/User/CurrentlyLoggedProfile';
-import Loading from '../src/components/Loading';
+} from "../src/features/DirectMessage/DirectMessageSlice";
+import { findProfiles } from "../src/features/Profile/FindUsernameByProfileSlice";
+import { fetchLoggedProfile } from "../src/features/User/CurrentlyLoggedProfile";
+import Loading from "../src/components/Loading";
 
 export default function DirectMessage() {
   // const [, setReceiverUsername] = useState("");
@@ -24,9 +24,9 @@ export default function DirectMessage() {
   const dispatch = useDispatch();
   const message = useSelector((state) => state.directMessages.allPrivMessage);
   const receiverUsername = useSelector((state) => state.receiver.username);
-  const [sendMessage, setSendMessage] = useState('');
+  const [sendMessage, setSendMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [fileName, setFileName] = useState('Upload');
+  const [fileName, setFileName] = useState("Upload");
   const [file, setFile] = useState(null);
   const { username } = useParams();
   const loggedProfile = useSelector(
@@ -44,14 +44,14 @@ export default function DirectMessage() {
 
   const handleSendMessage = async (event) => {
     event.preventDefault();
-    setLoading('Loading....');
+    setLoading("Loading....");
     const response = dispatch(
       sendPrivMessage(username, sendMessage, currentUser, file)
     ).then(() => {
       if (response) {
-        setFileName('Upload');
+        setFileName("Upload");
         setFile(null);
-        setSendMessage('');
+        setSendMessage("");
         setLoading(false);
       }
     });
@@ -63,19 +63,19 @@ export default function DirectMessage() {
 
   const handleFileUpload = (e) => {
     setFile(e.target.files[0]);
-    setFileName(e.target.files[0] ? e.target.files[0].name : 'Upload');
+    setFileName(e.target.files[0] ? e.target.files[0].name : "Upload");
   };
 
   const clearFile = (event) => {
     event.preventDefault();
     setFile(null);
-    setFileName('Upload');
+    setFileName("Upload");
   };
 
   // tambahkan ini di dalam fungsi DirectMessage
   useEffect(() => {
     socket.connect();
-    socket.on('broadcastMessage', (newMessage) => {
+    socket.on("broadcastMessage", (newMessage) => {
       // console.log(newMessage, "<<<< ini dari client");
       // setMessage((prevMessages) => [...prevMessages, newMessage]);
       if (currentUser.currentId === newMessage.receiver) {
@@ -85,7 +85,7 @@ export default function DirectMessage() {
       // console.log(message, "<<<<< ini message")
     });
 
-    socket.on('broadcastDelete', (data) => {
+    socket.on("broadcastDelete", (data) => {
       // setMessage(data)
       dispatch(fetchDirectMessages(username));
     });
@@ -94,8 +94,8 @@ export default function DirectMessage() {
 
     return () => {
       socket.disconnect();
-      socket.off('broadcastMessage');
-      socket.off('broadcastDelete');
+      socket.off("broadcastMessage");
+      socket.off("broadcastDelete");
     };
   }, [message]);
 
@@ -106,18 +106,18 @@ export default function DirectMessage() {
 
   return (
     <>
-      <div className='flex h-screen overflow-hidden '>
+      <div className="flex h-screen overflow-hidden ">
         {/* Sidebar */}
         <Sidebar />
 
         {/* Main Chat Area */}
-        <div className='flex-1 h-100vh'>
+        <div className="flex-1 h-screen">
           {/* Chat Header */}
-          <header className='bg-white p-4 text-gray-700'>
-            <h1 className='text-2xl font-semibold'>@{receiverUsername}</h1>
+          <header className="bg-white p-4 text-gray-700">
+            <h1 className="text-2xl font-semibold">@{receiverUsername}</h1>
           </header>
           {/* Chat Messages */}
-          <div className='h-screen max-h-[80vh] overflow-y-auto p-4 pb-36'>
+          <div className="h-full max-h-[80vh] overflow-y-auto p-4 pb-36">
             {message.map((el, index) => {
               return el.messageBelongsToLoggedUser == true ? (
                 <OutgoingMessage
@@ -147,47 +147,44 @@ export default function DirectMessage() {
             {/* Outgoing Message */}
           </div>
           {/* Chat Input */}
-          <footer className='bg-white border-t border-gray-300 p-4 absolute bottom-0 w-3/4 border-solid'>
+          <footer className="bg-white border-t border-gray-300 p-4 fixed bottom-0 w-3/4 border-solid">
             <div>
-              <form
-                onSubmit={handleSendMessage}
-                className='flex items-center'
-              >
+              <form onSubmit={handleSendMessage} className="flex items-center">
                 <input
-                  type='text'
-                  placeholder='Type a message...'
+                  type="text"
+                  placeholder="Type a message..."
                   value={sendMessage}
                   onChange={(e) => setSendMessage(e.target.value)}
-                  className='w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500 hover:shadow-md transition-shadow duration-300'
+                  className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500 hover:shadow-md transition-shadow duration-300"
                 />
                 <input
-                  type='file'
-                  id='upload'
-                  accept='image/*'
-                  className='hidden'
+                  type="file"
+                  id="upload"
+                  accept="image/*"
+                  className="hidden"
                   onChange={handleFileUpload}
                 />
                 <label
-                  htmlFor='upload'
-                  className='bg-blue-500 text-white px-4 py-2 rounded-md ml-2 cursor-pointer hover:bg-blue-800 transition-colors duration-300'
+                  htmlFor="upload"
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2 cursor-pointer hover:bg-blue-800 transition-colors duration-300"
                 >
                   {fileName}
                 </label>
                 <button
-                  type='submit'
-                  className='bg-indigo-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-indigo-800 transition-colors duration-300'
+                  type="submit"
+                  className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-indigo-800 transition-colors duration-300"
                 >
-                  {loading ? <Loading /> : 'Send'}
+                  {loading ? <Loading /> : "Send"}
                 </button>
                 <button
                   onClick={clearFile}
-                  className='bg-red-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-red-800 transition-colors duration-300'
+                  className="bg-red-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-red-800 transition-colors duration-300"
                 >
                   Clear
                 </button>
                 {/* <button type="submit" className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">
-                  Send
-                </button> */}
+          Send
+        </button> */}
               </form>
             </div>
           </footer>
