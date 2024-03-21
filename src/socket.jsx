e8aa66fd6384d43c0fbc27fe74d32f54826729d1
carloshakim00 <carloshakim00@gmail.com>
2024-03-21 10:01:41 +0700

    fix:socket

--- src/socket.jsx
+++ src/socket.jsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 const URL =
       process.env.NODE_ENV === "production"
       ? "https://appchats-bleach.carloshakim.online"
-      : "http://localhost:3000",
+      : "http://localhost:3000"
 
 export const socket = io(URL, {
   autoConnect: false,
